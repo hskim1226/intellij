9e99dfbbbad8893a82b2c291b4398cba5da9cb7f
김효숙 <hskim@apexsoft.co.kr>
2017-01-01 19:03:00 +0900

    덧셈

--- src/test.java
+++ src/test.java
@@ -5,8 +5,7 @@ public class test {
 
     public static void main(String[] args) {
 
-        System.out.println("덧셈");
-        System.out.println(add(4,5,6));
+        System.out.println("덧셈: "+add(4,5,6));
     }
 
     public static int add(int... arr) {
