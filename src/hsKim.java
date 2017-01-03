f017910c30e77be726aec64c69c63784358b1524
김효숙 <hskim@apexsoft.co.kr>
2017-01-03 12:12:40 +0900

    전체 수정

--- src/hsKim.java
+++ src/hsKim.java
@@ -1,13 +1,13 @@
 /**
  * Created by hskim@apexsoft.co.kr on 2017-01-01.
  */
-public class test {
+public class hsKim {
 
     public static void main(String[] args) {
 
         System.out.println("덧셈: " + add(4, 5, 6));
         System.out.println("뺄셈: " + sub(5, 4, 8));
-        System.out.println("나눗셈: " + div(10,2,15));
+        System.out.println("나눗셈: " + div(10, 2, 15));
         System.out.println("외국인 인가: " + new Application("F").isForeignAdmission());
 
     }
@@ -24,34 +24,33 @@ public class test {
     private static int sub(int... arr) {
         int re = 0;
         for (int num : arr) {
-            if (isbigNum(re, num)) {
+            if (re > num) {
                 re -= num;
             } else re = num - re;
         }
         return re;
     }
-    private static int div(int... arr){
+
+    private static int div(int... arr) {
         int re = 1;
         for (int num : arr) {
-            if (isbigNum(re, num)) {
-               re=re/num;
-            } else re = num/re;
+
+            if (re > num) {
+                re = re / num;
+            } else re = num / re;
         }
         return re;
 
     }
 
-    private static boolean isbigNum(int a, int b) {
-
-        return a > b;
-    }
 
     public static class Application {
         private String id;
         private String admissionType;
-        Application(String admissionType){
 
-            this.admissionType=admissionType;
+        Application(String admissionType) {
+
+            this.admissionType = admissionType;
         }
 
         public String getId() {
@@ -70,9 +69,9 @@ public class test {
             this.admissionType = admissionType;
         }
 
-        public boolean isForeignAdmission(){
+        public boolean isForeignAdmission() {
 
-            return "F".equals(getAdmissionType()) || "G".equals(getAdmissionType())||"H".equals(getAdmissionType())||"I".equals(getAdmissionType());
+            return "F".equals(getAdmissionType()) || "G".equals(getAdmissionType()) || "H".equals(getAdmissionType()) || "I".equals(getAdmissionType());
         }
     }
 
